f12462cabbcae0da7c8f6dcf271612665cd44840
Cees Timmerman <cees@shoppingminds.com>
2021-02-09 21:51:48 +0100

    fix vague var nag from linter

--- src/wind_turbine.py
+++ src/wind_turbine.py
@@ -30,9 +30,9 @@ circfill(66,50,3,6)rectfill(0,125,127,127,11)flip()goto _"""
 
 
 def _init():
-    global k, s, c, l
+    global k, s, c, L
     k = Table([7, 11, 3])
-    s, c, l = sin, cos, line
+    s, c, L = sin, cos, line
 
 
 def _update():
@@ -44,14 +44,14 @@ def _draw():
     for i in range(1, 80 + 1):
         o = c(i / 225) * 2
         p = max(flr(-o * 3), 0) + 1
-        l(64 + o, i + 50, 64 - o + 5, i + 50, k[p])
+        L(64 + o, i + 50, 64 - o + 5, i + 50, k[p])
 
     for i in range(0, 2 + 1):
         d = i / 3 + t() / 9
         j = -0.1
         while j <= 0.1:
             j += 0.01
-            l(66 + s(d + j) * 3, 50 + c(d + j) * 3, s(d) * 40 + 66, c(d) * 40 + 50, 7)
+            L(66 + s(d + j) * 3, 50 + c(d + j) * 3, s(d) * 40 + 66, c(d) * 40 + 50, 7)
 
     circfill(66, 50, 3, 6)
     rectfill(0, 125, 127, 127, 11)
