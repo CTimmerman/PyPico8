1d72dbf6e9a3ce0b57fe256f520abc0f9abbc014
Cees Timmerman <cees@shoppingminds.com>
2021-02-14 21:42:59 +0100

    tostring is only a Lua thing, not Pico8

--- src/pypico8/__init__.py
+++ src/pypico8/__init__.py
@@ -22,7 +22,7 @@ import pygame, pygame.freetype
 from .math import atan2, ceil, cos, div, flr, max, mid, min, rnd, sgn, sin, sqrt, srand  # noqa; unused here but maybe not elsewhere.
 from .table import Table, add, all, delete, foreach, pairs, pack, unpack  # noqa
 from .audio import audio_channel_notes, music, sfx, threads  # noqa
-from .strings import chr, ord, pico8_to_python, printh, split, sub, tonum, tostr, tostring  # noqa
+from .strings import chr, ord, pico8_to_python, printh, split, sub, tonum, tostr  # noqa
 from .video import  _init_video, camera, circ, circfill, clip, cls, color, cursor, fget, fillp, flip, frame_count, fset, get_char_img, line, map, memcpy, mget, mset, multimethod, oval, ovalfill, pal, palt, peek, peek2, peek4, pget, poke, poke2, poke4, pos, print, pset, rect, rectfill, replace_color, reset, sget, spr, sset, sspr, to_col  # noqa
 #fmt:on
 false = False
