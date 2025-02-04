f8736175f91d9fa6629b1085f0002cbe0f0901b3
Cees Timmerman <CTimmerman@users.noreply.github.com>
2025-02-04 20:27:34 +0100

    Add original octopus code.

--- src/octopus.py
+++ src/octopus.py
@@ -1,4 +1,17 @@
 """Octopus ported from https://twitter.com/picoter8/status/1330759076943376384
+-- octopus #pico8 #tweetcart #tweetjam
+::_::cls()
+for r=16,128,6do
+for a=0,1,1/r do
+b=a+.1*(1-r/96)*sin(.8*t()+r/96)
+x=62+(r+8*sin(t()/5))*cos(b)
+y=62+(r+8*sin(t()/5))*sin(b)
+q=3*sin(a*8-t()/5)
+circfill(x,y,q+2,2)circfill(x,y,q,1)
+end
+end
+flip()goto _
+
 Purple/blue tentacles in the dark.
 """
 
